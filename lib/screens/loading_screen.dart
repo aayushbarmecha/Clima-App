fe178bd8b9465feef56e3fdb6fbde2b62ee696b4
Aayush Barmecha <68626644+aayushbarmecha@users.noreply.github.com>
2023-08-19 22:33:52 +0530

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/get_location_weather.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,16 +10,41 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocationData();
+//    switchToLocationScreen();
+  }
+
+  void getLocationData() async {
+    LocationWeather theLocationWeather = LocationWeather();
+    dynamic weatherData;
+    weatherData = await theLocationWeather.getLocationWeather();
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => LocationScreen(
+                  data: weatherData,
+                )));
+//    });
+  }
+
+//  void switchToLocationScreen() {
+//
+//  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
+      body: Container(
+        child: SpinKitRotatingCircle(
+          size: 100.0,
+          color: Colors.white,
         ),
       ),
     );
   }
 }
+
+//Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) =>LoadingScreen()));
